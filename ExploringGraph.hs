57da6796962f98890f7be901a2fd233583cb7ea5
DF <leegbestand@gmail.com>
2020-12-31 14:03:39 +0100

    Remove case over bools and change explore -> execute.

--- ExploringGraph.hs
+++ ExploringGraph.hs
@@ -2,6 +2,8 @@
 
 module ExploringGraph where
 
+import Data.List
+
 
 data Graph programs configs = Graph {edges :: [(configs, programs, configs)], nodes :: [configs]} deriving (Show)
 
@@ -10,14 +12,12 @@ buildGraph edges nodes = Graph {edges = edges, nodes = nodes}
 
 graphAddVertices :: Eq v => Graph e v -> [v] -> Graph e v
 graphAddVertices g [] = g
-graphAddVertices g (v:vs) = case elem v (nodes g) of
-    False -> graphAddVertices (g {nodes = nodes g ++ [v]}) vs
-    True -> graphAddVertices g vs
+graphAddVertices g (v:vs) = 
+    if v `elem` nodes g then graphAddVertices (g {nodes = nodes g ++ [v]}) vs else graphAddVertices g vs
 
 graphAddEdge :: Eq e => Eq n => Graph e n -> (n, e, n) -> Graph e n
-graphAddEdge g (s, e, t) = case elem (s, e, t) (edges g) of
-    True -> g
-    False -> graphAddVertices (g {edges = edges g ++ [(s, e, t)]}) [s, t]
+graphAddEdge g (s, e, t) = 
+    if (s, e, t) `elem` edges g then g else graphAddVertices (g {edges = edges g ++ [(s, e, t)]}) [s, t]
 
 displayGraphEdgeDot :: Show v => Show e => (v, e, v) -> IO ()
 displayGraphEdgeDot (s, e, t) = do
@@ -35,25 +35,22 @@ displayGraphDot g = do
     putStrLn "}"
 
 
-
--- Currently uses a list(stack) as the execution graph.
 data Explorer programs configs = Explorer { defInterp :: programs -> configs -> configs, config :: configs, execEnv :: Graph programs configs}
 
 -- Constructor for a exploring interpreter.
 build :: (a -> b -> b) -> b -> Explorer a b
 build definterp conf = Explorer {defInterp = definterp, config = conf , execEnv = buildGraph [] [conf]}
 
-explore :: Eq p => Eq c => Explorer p c -> p -> Explorer p c
-explore int prog = int { config = newconfig, execEnv = graphAddEdge (execEnv int) (oldconfig, prog, newconfig)}
+
+execute :: Eq p => Eq c => Explorer p c -> p -> Explorer p c
+execute int prog = case find (\(c, p, _) -> c == oldconfig && p == prog) (edges $ execEnv int) of
+    Just (curr, _, new) -> int {config = new}
+    Nothing -> int { config = newconfig, execEnv = graphAddEdge (execEnv int) (oldconfig, prog, newconfig)}
     where oldconfig = config int
           newconfig = defInterp int prog oldconfig
-
-displayEdge :: Show c => Show p => (c, p) ->  String
-displayEdge (c1, p) = show c1 ++ "\n" ++ "|\n" ++ "|\n| " ++ show p ++ "\n|\n|\nv\n"
 
 display :: Show c => Show p => Explorer p c -> IO ()
 display explorer = displayGraphDot (execEnv explorer)
 
--- Destroys future steps(relative to the y in revert) when reverting, but keeps the history(before the y in revert) alive.
 revert :: Eq c => Eq p => Explorer p c -> c -> Explorer p c
 revert explorer newconfig = explorer { config = newconfig, execEnv = graphAddVertices (execEnv explorer) [newconfig]}
