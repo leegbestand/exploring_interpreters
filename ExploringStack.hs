79f27d42ef63ac39c9038ff79f1a8e8cd627636b
DF <leegbestand@gmail.com>
2020-12-31 13:47:08 +0100

    Flip the Stack Env to actually work like a stack.
    This makes the revert much cleaner, thanks to Thomas.

--- ExploringStack.hs
+++ ExploringStack.hs
@@ -11,22 +11,24 @@ data Explorer programs configs = Explorer { defInterp :: programs -> configs -> 
 build :: (a -> b -> b) -> b -> Explorer a b
 build definterp conf = Explorer {defInterp = definterp, config = conf , execEnv = []}
 
-explore :: Explorer p c -> p -> Explorer p c
-explore int prog = int { config = newconfig, execEnv = execEnv int ++ [(oldconfig, prog)]}
+execute :: Explorer p c -> p -> Explorer p c
+execute int prog = int { config = newconfig, execEnv = (oldconfig, prog) : execEnv int}
     where oldconfig = config int
           newconfig = defInterp int prog oldconfig
 
 displayEdge :: Show c => Show p => (c, p) ->  String
 displayEdge (c1, p) = show c1 ++ "\n" ++ "|\n" ++ "|\n| " ++ show p ++ "\n|\n|\nv\n"
 
+
 display :: Show c => Show p => Explorer p c -> IO ()
-display explorer = case execEnv explorer of
-    [] -> print (config explorer)
-    _ -> putStrLn $ concatMap displayEdge (execEnv explorer) ++ show (config explorer)
+display explorer =  case currExecEnv of
+    [] -> putStrLn $ show config explorer
+    _  -> putStrLn $ concatMap displayEdge currExecEnv ++ show (config explorer)
+    where currExecEnv = execEnv explorer
 
 
 -- Destroys future steps(relative to the y in revert) when reverting, but keeps the history(before the y in revert) alive.
 revert :: Eq c => Explorer p c -> c -> Explorer p c
-revert explorer newconfig = explorer { config = newconfig, execEnv = newgraph }
-    where newgraph = if length prunedGraph == length (execEnv explorer) then [] else prunedGraph
-          prunedGraph = takeWhile (\(c, _) -> c /= newconfig) (execEnv explorer)
+revert explorer newconfig = if newconfig == config explorer then explorer else
+    explorer { config = newconfig, execEnv = newgraph }
+    where (_:newgraph) = dropWhile (\(c, _) -> c /= newconfig) (execEnv explorer)
